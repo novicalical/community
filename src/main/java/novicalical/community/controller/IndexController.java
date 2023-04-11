2c44e7d65e57b2f819014a281ce3c3c86d8c0212
novicalical <novicalical@gmail.com>
2023-04-11 16:41:33 +0800

    add index and bootstrap

---
+++ src/main/java/novicalical/community/controller/IndexController.java
@@ -0,0 +1,14 @@
+package novicalical.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by novicalical on 2023/4/10
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){ return"index";}
+    }
+
