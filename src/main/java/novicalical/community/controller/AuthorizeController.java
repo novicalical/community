5ae6255f22f1f589f5579a5d4f2e5753556d5a5d
novicalical <novicalical@gmail.com>
2023-04-14 12:40:46 +0800

    extract github config from code to properties

---
+++ src/main/java/novicalical/community/controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package novicalical.community.controller;
+
+import novicalical.community.dto.AccessTokenDTO;
+import novicalical.community.dto.GithubUser;
+import novicalical.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by novicalical on 2023/4/10
+ */
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("$(github.client.id)")
+    private String clientId;
+
+    @Value("$(github.client.secret)")
+    private String clientSecret;
+
+    @Value("$(github.redirect.uri)")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="state") String state){
+        final AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
---
+++ src/main/java/novicalical/community/provider/GithubProvider.java
@@ -0,0 +1,45 @@
+package novicalical.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import novicalical.community.dto.AccessTokenDTO;
+import novicalical.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+/**
+ * Created by novicalical on 2023/4/10
+ */
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
