023900705ff813a709acc00e615d898791785a70
novicalical <novicalical@gmail.com>
2023-04-17 13:35:25 +0800

    add check cookies

--- src/main/java/novicalical/community/controller/IndexController.java
+++ src/main/java/novicalical/community/controller/IndexController.java
@@ -1,13 +1,36 @@
 package novicalical.community.controller;
 
+import novicalical.community.mapper.UserMapper;
+import novicalical.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by novicalical on 2023/4/10
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){ return"index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+               String token = cookie.getValue();
+               User user = userMapper.findByToken(token);
+               if(user != null){
+                   request.getSession().setAttribute("user",user);
+               }
+               break;
+            }
+        }
+        return"index";
     }
+}
 
--- src/main/java/novicalical/community/mapper/UserMapper.java
+++ src/main/java/novicalical/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package novicalical.community.mapper;
 import novicalical.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by novicalical on 2023/4/10
@@ -11,4 +13,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
